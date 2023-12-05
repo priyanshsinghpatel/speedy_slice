8cd2f2b5495e33e08d3a21acb2ee7db6d3568df9
priyanshsinghpatel <patelpsp206@gmail.com>
2023-12-05 19:49:41 +0530

    Project Completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+
 const Navbar = () => {
+    const quantity = useSelector ((state)=> state.cart.quantity);
     return (
         <div className={styles.container}>
             <div className={styles.item}>
@@ -15,7 +19,9 @@ const Navbar = () => {
             </div>
             <div className={styles.item}>
                 <ul className={styles.list}>
+                    <Link href="/" passHref>
                     <li className={styles.listItem}>Homepage</li>
+                    </Link>                    
                     <li className={styles.listItem}>Products</li>
                     <li className={styles.listItem}>Menu</li>
                     <Image src= "/img/logo.png" alt ="" width="160" height="160"></Image>
@@ -24,12 +30,16 @@ const Navbar = () => {
                     <li className={styles.listItem}>Contact</li>
                 </ul>
             </div>
-            <div className={styles.item}>
-                <div className={styles.cart}>
-                <Image src="/img/cart_logo.png" alt ="" width="160" height="160"></Image>
-                <div className={styles.counter}>0</div>
+            <Link href="/cart" passHref>
+             <div className={styles.item}>
+                <div>
+                {/* <Image src="/img/cart_logo.png" alt ="" width="160" height="160"></Image> */}
+                <div>cart({quantity})</div>
                 </div>
-            </div>    
+             </div>    
+            </Link>
+            
+            
         </div>
     );
 };
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -18,7 +18,9 @@ export default async function handler(req,res){
    }
    if(method==="PUT"){
       try {
-         const product= await Product.create(req.body);
+         const product= await Product.findByIdAndUpdate(id, req.body, {
+            new:true,
+         });
          res.status(200).json(product)
       } catch (err) {
          res.status(500).json(err);         
@@ -26,8 +28,8 @@ export default async function handler(req,res){
    }
    if(method==="DELETE"){
     try {
-       const product= await Product.create(req.body);
-       res.status(200).json(product)
+       const product= await Product.findByIdAndDelete(id);
+       res.status(200).json("The product has been deleted!");
     } catch (err) {
        res.status(500).json(err);         
     }
